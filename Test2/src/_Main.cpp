b83a942823811cdd00764ecf486e252cd3816769
Vlad-07 <vladup07@gmail.com>
2022-01-24 19:22:25 +0200

    h - SAFE

--- Test2/src/_Main.cpp
+++ Test2/src/_Main.cpp
@@ -16,6 +16,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 #include "Renderer.h"
+
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
@@ -23,47 +24,18 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "tests/TestClearColor.h"
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
 #define WindowWidth 960
 #define WindowHeight 540
 
-double Map(double x, double in_min, double in_max, double out_min, double out_max)
+static double Map(double x, double in_min, double in_max, double out_min, double out_max)
 {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-
-inline void CheckGLFW()
-{
-	if (!glfwInit())
-		std::cout << "GLFW...........................FAILED\n";
-	else
-		std::cout << "GLFW...........................OK\n";
-}
-
-inline void CheckGLEW()
-{
-	if (glewInit() != GLEW_OK)
-		std::cout << "GLEW...........................FAILED\n";
-	else
-		std::cout << "GLEW...........................OK\n";
-}
-
-inline void CheckWindow(GLFWwindow* window)
-{
-	if (!window)
-	{
-		std::cout << "GLFW_window_init...............FAILED\n";
-		std::cin.get();
-		glfwTerminate();
-		exit(-1);
-	}
-	std::cout << "GLFW_WINDOW....................OK\n";
-}
-
-
 struct Random
 {
 private:
@@ -84,38 +56,69 @@ public:
 };
 
 
+static inline void CheckGLFW()
+{
+	if (!glfwInit())
+		std::cout << "GLFW...........................FAILED\n";
+	else
+		std::cout << "GLFW...........................OK\n";
+}
+static inline void CheckGLEW()
+{
+	if (glewInit() != GLEW_OK)
+		std::cout << "GLEW...........................FAILED\n";
+	else
+		std::cout << "GLEW...........................OK\n";
+}
+static inline void InitImGui(GLFWwindow* window)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
+	ImGui::StyleColorsDark();
+}
+static inline void CheckWindow(GLFWwindow* window)
+{
+	if (!window)
+	{
+		std::cout << "GLFW_window_init...............FAILED\n";
+		std::cin.get();
+		glfwTerminate();
+		exit(-1);
+	}
+	std::cout << "GLFW_WINDOW....................OK\n";
+}
+
+
 float MouseX = 0, MouseY = 0;
 
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
 	MouseX = xpos;
 	MouseY = ypos;
 }
-
 
 int main(void)
 {
 	std::cout << "Begin...\n\n";
 
 	CheckGLFW();
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 16);
 
 	GLFWwindow* window;
 	GLFWmonitor* primary = glfwGetPrimaryMonitor();
-	
-	glfwWindowHint(GLFW_SAMPLES, 16);
 	window = glfwCreateWindow(WindowWidth, WindowHeight, "Window", NULL, NULL);
-
 	CheckWindow(window);
+	glfwMakeContextCurrent(window);
 
 	glfwSetCursorPosCallback(window, cursor_position_callback);
 
-	glfwMakeContextCurrent(window);
-
 	CheckGLEW();
+
 
 	GLCall(glEnable(GL_BLEND));
 	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
@@ -124,56 +127,13 @@ int main(void)
 	GLCall(glEnable(GL_MULTISAMPLE));
 	GLCall(glfwSwapInterval(1));
 
-
-
-	float vertices[] = {
-		-50.0f, -50.0f, 0.0f,  0.0f, 0.0f,
-		 50.0f, -50.0f, 0.0f,  1.0f, 0.0f,
-		 50.0f,  50.0f, 0.0f,  1.0f, 1.0f,
-		-50.0f,  50.0f, 0.0f,  0.0f, 1.0f
-	};
-
-	unsigned int indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-
-
-	VertexArray va;
-	VertexBuffer vb(vertices, 4 * 5 * sizeof(float));
-	VertexBufferLayout layout;
-	layout.Push<float>(3);
-	layout.Push<float>(2);
-	va.AddBuffer(vb, layout);
-
-	IndexBuffer ib(indices, 2 * 3 * sizeof(float));
 	
-
-	glm::mat4 proj = glm::ortho(0.0f, (float)WindowWidth, 0.0f, (float)WindowHeight, -1.0f, 1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-	Shader shader("res/shaders/basic.shader");
-
-	shader.Bind();
-
-	Texture tex("res/textures/image.png");
-	tex.Bind();
-	shader.SetUniform1i("u_Texture", 0);
+	InitImGui(window);
 
 	Renderer renderer;
 
+	test::ClearColor test;
 
-
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330 core");
-	ImGui::StyleColorsDark();
-
-
-	glm::vec3 translationA(0, 0, 0);
-	glm::vec3 translationB(0, 0, 0);
 
 	std::cout << "\n\n\n";
 	while (!glfwWindowShouldClose(window))
@@ -183,29 +143,11 @@ int main(void)
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
+		
+		test.OnUpdate(0);
+		test.OnRender();
+		test.OnImguiRender();
 
-			renderer.Draw(va, ib, shader);
-		}
-
-		{
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-			glm::mat4 mvp = proj * view * model;
-			shader.Bind();
-			shader.SetUniformMat4f("u_MVP", mvp);
-
-			renderer.Draw(va, ib, shader);
-		}
-
-		ImGui::Begin("Debug");
-		ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-		ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::End();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
